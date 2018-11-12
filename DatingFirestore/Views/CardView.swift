2978732ebc9ffc70ce544d689417f14d77452437
Azhar <rahzadesign@gmail.com>
2018-11-12 16:40:10 +0530

    setup pan gesture recognizer for the card view

---
+++ DatingFirestore/Views/CardView.swift
@@ -0,0 +1,56 @@
+//
+//  CardView.swift
+//  DatingFirestore
+//
+//  Created by Azhar Anwar on 11/12/18.
+//  Copyright © 2018 Azhar Anwar. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    fileprivate let imageView = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        //custom drawing code
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        addSubview(imageView)
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.fillSuperview()
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        addGestureRecognizer(panGesture)
+    }
+    
+    @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .changed:
+            handleChanged(gesture)
+        case .ended:
+            handleEnded()
+        default:
+            ()
+        }
+    }
+    
+    fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: nil)
+        self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+    }
+    
+    fileprivate func handleEnded() {
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+            self.transform = .identity
+        }) { (_) in
+            //
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

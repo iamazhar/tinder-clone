a68d0f0b3662c0ad8f1cae6cddb7aa0d35a193ca
Azhar <rahzadesign@gmail.com>
2018-11-13 18:06:09 +0530

    added protocol to card view; refactored code in home controller; encapsulated card view properties

---
+++ DatingFirestore/Model/Advertiser.swift
@@ -0,0 +1,23 @@
+//
+//  Advertiser.swift
+//  DatingFirestore
+//
+//  Created by Azhar Anwar on 11/13/18.
+//  Copyright © 2018 Azhar Anwar. All rights reserved.
+//
+
+import UIKit
+
+struct Advertiser: ProducesCardViewModel {
+    let title: String
+    let brandName: String
+    let posterPhotoName: String
+    
+    func toCardViewModel() -> CardViewModel {
+        let attributedString = NSMutableAttributedString(string: title, attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 34, weight: .heavy)])
+        attributedString.append(NSMutableAttributedString(string: "\n\(brandName)", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 24, weight: .bold)]))
+        
+        return CardViewModel(imageName: posterPhotoName, attributedText: attributedString, textAlignment: .center)
+    }
+}
+

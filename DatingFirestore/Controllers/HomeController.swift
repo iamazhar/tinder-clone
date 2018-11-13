600969b1a299eea09e9cce84a9b8db6b81bbae86
Azhar <rahzadesign@gmail.com>
2018-11-13 16:37:22 +0530

    added mvvm pattern with card view model

--- DatingFirestore/Controllers/HomeController.swift
+++ DatingFirestore/Controllers/HomeController.swift
@@ -14,31 +14,28 @@ class HomeController: UIViewController {
     let cardsDeckView = UIView()
     let buttonsStackView = HomeBottomControlsStackView()
     
-    let users = [
-        User(name: "Kelly", age: 23, profession: "Music/DJ", imageName: "lady5c"),
-        User(name: "Jane", age: 18, profession: "Teacher", imageName: "lady4c")
+//    let users = [
+//        User(name: "Kelly", age: 23, profession: "Music/DJ", imageName: "lady5c"),
+//        User(name: "Jane", age: 18, profession: "Teacher", imageName: "lady4c")
+//    ]
+    
+    let cardViewModels = [
+        User(name: "Kelly", age: 23, profession: "Music/DJ", imageName: "lady5c").toCardViewModel(),
+        User(name: "Jane", age: 18, profession: "Teacher", imageName: "lady4c").toCardViewModel()
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setupLayout()
         setupDummyCards()
     }
     
     fileprivate func setupDummyCards() {
-        users.forEach { (user) in
+        cardViewModels.forEach { (cardViewModel) in
             let cardView = CardView(frame: .zero)
-            cardView.imageView.image = UIImage(named: user.imageName)
-            cardView.informationLabel.text = "\(user.name) \(user.age)\n\(user.profession)"
-            
-            let attributedText = NSMutableAttributedString(string: user.name, attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 32, weight: .heavy)])
-            
-            attributedText.append(NSAttributedString(string: "  \(user.age)", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 24, weight: .regular)]))
-            
-            attributedText.append(NSAttributedString(string: "\n\(user.profession)", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 20, weight: .regular)]))
-            
-            cardView.informationLabel.attributedText = attributedText
+            cardView.imageView.image = UIImage(named: cardViewModel.imageName)
+            cardView.informationLabel.attributedText = cardViewModel.attributedText
+            cardView.informationLabel.textAlignment = cardViewModel.textAlignment
             
             cardsDeckView.addSubview(cardView)
             cardView.fillSuperview()
---
+++ DatingFirestore/ViewModels/CardViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  CardViewModel.swift
+//  DatingFirestore
+//
+//  Created by Azhar Anwar on 11/13/18.
+//  Copyright © 2018 Azhar Anwar. All rights reserved.
+//
+
+import UIKit
+
+struct CardViewModel {
+    //Defining the properties the view will render out
+    let imageName: String
+    let attributedText: NSAttributedString
+    let textAlignment: NSTextAlignment
+}
+
+

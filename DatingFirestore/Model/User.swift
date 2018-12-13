5121759ca9aac1fef99a5ba74ccff89d201d69a5
Azhar <rahzadesign@gmail.com>
2018-12-13 14:06:51 +0530

    saving three images to forebase storage and loading them; added code to handle correct display of single or multiple images on the user card

--- DatingFirestore/Model/User.swift
+++ DatingFirestore/Model/User.swift
@@ -13,6 +13,8 @@ struct User: ProducesCardViewModel {
     var age: Int?
     var profession: String?
     var imageUrl1: String?
+    var imageUrl2: String?
+    var imageUrl3: String?
     var uid: String?
     
     init(dictionary: [String: Any]) {
@@ -20,7 +22,9 @@ struct User: ProducesCardViewModel {
         self.age = dictionary["age"] as? Int
         self.profession = dictionary["profession"] as? String
         self.name = dictionary["fullName"] as? String ?? ""
-        self.imageUrl1 = dictionary["imageUrl1"] as? String ?? ""
+        self.imageUrl1 = dictionary["imageUrl1"] as? String
+        self.imageUrl2 = dictionary["imageUrl2"] as? String
+        self.imageUrl3 = dictionary["imageUrl3"] as? String
         self.uid = dictionary["uid"] as? String ?? ""
     }
     
@@ -34,7 +38,11 @@ struct User: ProducesCardViewModel {
         
         attributedText.append(NSAttributedString(string: "\n\(professionString)", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 20, weight: .regular)]))
         
-        return CardViewModel(imageNames: [imageUrl1 ?? ""], attributedString: attributedText, textAlignment: .left)
+        var imageUrls = [String]()
+        if let url = imageUrl1 { imageUrls.append(url) }
+        if let url = imageUrl2 { imageUrls.append(url) }
+        if let url = imageUrl3 { imageUrls.append(url) }
+        return CardViewModel(imageNames: imageUrls, attributedString: attributedText, textAlignment: .left)
     }
 }
 
